3e31d965435fd221738f49c543ddbe998b1348f8
IveJ <datacentralize@gmail.com>
2019-05-09 12:42:45 +0700

    Create fr.py

---
+++ fr.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot                                  
+from matplotlib.patches import Rectangle
+from mtcnn.mtcnn import MTCNN
+# load image from file
+pixels = pyplot.imread('storage/emulated/0/Download/street.jpg')
+# create the detector, using default weights
+detector = MTCNN()
+# detect faces in the image
+faces = detector.detect_faces(pixels)
+# plot the image                                               pyplot.imshow(pixels)
+# get the context for drawing boxes
+ax = pyplot.gca()
+# get coordinates from the first face
+x, y, width, height = faces[0]['box']
+# create the shape
+rect = Rectangle((x, y), width, height, fill=False, color='red')
+# draw the box
+ax.add_patch(rect)
+# show the plot
+pyplot.show()
